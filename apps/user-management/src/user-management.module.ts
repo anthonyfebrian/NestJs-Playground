67139a6c035b7dd4706e2eff9e7a325193f1988c
brian <brian@ezeelink.co.id>
2025-02-26 20:47:25 +0700

    init users, auth, roles module on user management

--- apps/user-management/src/user-management.module.ts
+++ apps/user-management/src/user-management.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserManagementController } from './user-management.controller';
 import { UserManagementService } from './user-management.service';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RolesModule, AuthModule],
   controllers: [UserManagementController],
   providers: [UserManagementService],
 })
