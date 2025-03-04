a29a82c759816a77fbcbf8af83481f2889e77682
brian <brian@ezeelink.co.id>
2025-03-04 09:27:21 +0700

    add nestjs/config libs

--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ApiGatewayService {
   getHello(): string {
-    return 'Hello World from api gateway!';
+    return 'Hello World from api gateway!' + process.env.DATABASE_USER;
   }
 }
