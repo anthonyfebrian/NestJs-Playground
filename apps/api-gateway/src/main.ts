27e7c2c845d96b81436095127291397e44a1772f
brian <brian@ezeelink.co.id>
2025-03-06 08:58:26 +0700

    Enable API versioning in the gateway using URI versioning strategy

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.enableVersioning(
+    {
+      type: VersioningType.URI,
+      defaultVersion: '1.0',
+    }
+  )
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
